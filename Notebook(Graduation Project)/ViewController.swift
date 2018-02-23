2d914c7fc2f89b135959518999919e26111b06ca
梁琛 <liangc@yxt.com>
2018-02-23 09:32:20 +0800

    Initial Commit

---
+++ Notebook(Graduation Project)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notebook(Graduation Project)
+//
+//  Created by Colick on 2018/2/23.
+//  Copyright © 2018年 The Big Nerd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
